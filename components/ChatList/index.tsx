c99d1a526feb8c1dd68c905bcdbac39fda039ba7
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-08-22 15:36:59 +0900

    DM 페이지 만들기 완료 404 오류해결 x

---
+++ components/ChatList/index.tsx
@@ -0,0 +1,11 @@
+import { ChatZone, Section } from '@components/ChatList/styles';
+import React from 'react';
+const ChatList = () => {
+  return (
+    <ChatZone>
+      <Section>Section</Section>
+    </ChatZone>
+  );
+};
+
+export default ChatList;
