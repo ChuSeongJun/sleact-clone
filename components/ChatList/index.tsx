67da0c5fb8964051afbc1f15c1a4fe344681186d
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-08-23 23:52:04 +0900

    DM 표시하기

--- components/ChatList/index.tsx
+++ components/ChatList/index.tsx
@@ -1,9 +1,17 @@
 import { ChatZone, Section } from '@components/ChatList/styles';
-import React from 'react';
-const ChatList = () => {
+import React, { VFC } from 'react';
+import { IDM } from '@typings/db';
+import Chat from '@components/Chat';
+
+interface Props {
+  chatData?: IDM[];
+}
+const ChatList: VFC<Props> = ({ chatData }) => {
   return (
     <ChatZone>
-      <Section>Section</Section>
+      {chatData?.map((chat) => (
+        <Chat key={chat.id} data={chat} />
+      ))}
     </ChatZone>
   );
 };
