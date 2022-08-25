f99c2f7abafae86f7b07cf4c378f0ada0ecdadbd
ChuSeongJun <91831079+ChuSeongJun@users.noreply.github.com>
2022-08-25 22:21:52 +0900

    정규표현식으로 문자열 반환

--- components/Chat/index.tsx
+++ components/Chat/index.tsx
@@ -1,15 +1,41 @@
-import React, { VFC } from 'react';
+import React, { VFC, memo, useMemo } from 'react';
 import { IDM } from '@typings/db';
 import { ChatWrapper } from '@components/Chat/styles';
 import gravatar from 'gravatar';
 import dayjs from 'dayjs';
+import regexifyString from 'regexify-string';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router';
 
 interface Props {
   data: IDM;
 }
 
 const Chat: VFC<Props> = ({ data }) => {
+  const { workspace } = useParams<{ workspace: string; channel: string }>();
   const user = data.Sender;
+  //@[추성준1](8)
+  // \d 숫자   +는 1개 이상  ? 는 0개 이상  g는 모두 찾기
+  // +? 최대한 조금
+  const result = useMemo(
+    () =>
+      regexifyString({
+        input: data.content,
+        pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
+        decorator(match, index) {
+          const arr = match.match(/@\[(.+?)]\((\d+?)\)/)!;
+          if (arr) {
+            return (
+              <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+                @{arr[1]}
+              </Link>
+            );
+          }
+          return <br key={index} />;
+        },
+      }),
+    [data.content],
+  );
   return (
     <ChatWrapper>
       <div className="chat-img">
@@ -20,9 +46,9 @@ const Chat: VFC<Props> = ({ data }) => {
           <b>{user.nickname}</b>
           <span>{dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
-        <p>{data.content}</p>
+        <p>{result}</p>
       </div>
     </ChatWrapper>
   );
 };
-export default Chat;
+export default memo(Chat);
